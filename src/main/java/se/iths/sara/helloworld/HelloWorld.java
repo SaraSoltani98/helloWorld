4e1e93cc5e48e2a45146a2a9262865553beafa70
SaraSoltani98 <SaraSoltani.SaraSoltani@iths.se>
2025-10-07 14:07:12 +0200

    testing...

---
+++ src/main/java/se/iths/sara/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package se.iths.sara.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String[] word = {"Hello", "World", "!"};
+
+        for (int i = 0; i < word.length; i++) {
+            System.out.print(word[i] + " ");
+        }
+    }
+}
